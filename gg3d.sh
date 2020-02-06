ab18077e44435763eddda37deede502a350e75f5
smhah <57076360+smhah@users.noreply.github.com>
2020-02-06 11:04:21 +0100

    Add files via upload

--- gg3d.sh
+++ gg3d.sh
@@ -1,1 +1,1 @@
-cc -I /usr/local/include cub3d.c -L /usr/local/lib -lmlx -framework OpenGl -framework Appkit utils/*.c utils/*.h -D BUFFER_SIZE=300 -g
+cc -I /usr/local/include cub3d.c file.c -L /usr/local/lib -lmlx -framework OpenGl -framework Appkit utils/*.c utils/*.h -D BUFFER_SIZE=300 -g
